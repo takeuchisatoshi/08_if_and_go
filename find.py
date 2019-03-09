c48bed684d9176edd854f0372a314597f3d54792
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 16:36:45 +0900

    不要行の削除

--- find.py
+++ find.py
@@ -1,9 +1,24 @@
 numbers = [34, 432, 1, 99]
 
+
 total = 0
 
 for number in numbers:
-
     total += number
-
 print(total)
+
+
+max_number = 0
+
+for number in numbers:
+    if max_number < number:
+        max_number = number
+print(max_number)
+
+
+minimum_number = 999
+
+for number in numbers:
+    if minimum_number > number:
+        minimum_number = number
+print(minimum_number)
