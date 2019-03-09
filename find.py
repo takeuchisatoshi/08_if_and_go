5d0ffdfa991a7cd83f510f17eb6724a890e5862b
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 16:37:54 +0900

    コメントの追加

--- find.py
+++ find.py
@@ -1,13 +1,13 @@
 numbers = [34, 432, 1, 99]
 
-
+# 合計値
 total = 0
 
 for number in numbers:
     total += number
 print(total)
 
-
+# 最大値
 max_number = 0
 
 for number in numbers:
@@ -15,7 +15,7 @@ for number in numbers:
         max_number = number
 print(max_number)
 
-
+# 最小値
 minimum_number = 999
 
 for number in numbers:
