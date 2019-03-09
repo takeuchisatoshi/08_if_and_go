8671a481ef4ea3ee0c227dd19b404224b5967ddb
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 17:34:26 +0900

    ファイル分けして整理

---
+++ for.py
@@ -0,0 +1,16 @@
+# for i in range(10):
+#     print(i)
+#
+# for i in range(11):
+#     print(i)
+#
+# for i in range(3, 11):
+#     print(i)
+
+for i in range(21):
+    if i % 2 != 0:
+        print(i)
+
+for i in range(21):
+    if i % 2 == 1:
+        print(i)
